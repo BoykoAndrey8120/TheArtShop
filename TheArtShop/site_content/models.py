c113cb47b42e8bc02bf591e55f3e702506a54542
andreyboyko <boyko8120@gmail.com>
2023-04-07 01:32:14 +0300

    Create project

---
+++ TheArtShop/site_content/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    title_category = models.CharField(max_length=255)
+    slug_category = models.SlugField(max_length=100, db_index=True, unique=True)
+    image_category = models.ImageField(upload_to='image/categories/', blank=True)
+
+
+class Brand(models.Model):
+    title_brand = models.CharField(max_length=255)
+    slug_brand = models.SlugField(max_length=100, db_index=True, unique=True)
+    image_brand = models.ImageField(upload_to='image/brands/', blank=True)
+
+
+class Product(models.Model):
+    title_product = models.CharField(max_length=255)
+    description_product = models.TextField(max_length=1000, blank=True)
+    slug_product = models.SlugField(max_length=100, db_index=True, unique=True)
+    price_product = models.DecimalField(max_digits=10, decimal_places=2)
+    image_product = models.ImageField(upload_to='image/products/', blank=True)
+    category_product = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    created = models.DateField(auto_now=True)
+    brand_product = models.ForeignKey(Brand, related_name='product', on_delete=models.CASCADE)
+    uploaded = models.DateField(auto_now=True)
+
+
+
+
