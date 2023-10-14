38e81db11fda2f3d3048ebeba352e7bef8f7e569
andreyboyko <boyko8120@gmail.com>
2023-10-15 00:17:59 +0300

    added views and urls

--- TheArtShop/site_content/urls.py
+++ TheArtShop/site_content/urls.py
@@ -1,12 +1,26 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView, LoginView
 
-from . import views
+from .views import CategoryViews, ProductsViews, category_with_products_list, ProductView, CustomLoginView, \
+    CustomLogoutView, register, Home
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('register/', register, name='register'),
+    path("login/", CustomLoginView.as_view(), name='login'),
+    # path('login/', views.login, name='login'),
+    # path('logout/', views.logout, name='logout'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
+    path("", Home.as_view(), name="home"),
+    path("<slug:slug_category>/", CategoryViews.as_view(), name="Category-detail"),
+    path("<slug:slug_category>/products/page/<int:page>/", ProductsViews.as_view(), name='product-list-paginated'),
+    path("<slug:slug_category>/products/", ProductsViews.as_view(), name="category_products", kwargs={'template_name': 'category_products.html'}),
+    path("<slug:slug_category>/<slug:slug_product>/<int:pk>/", ProductView.as_view(), name='product_detail'),
+    path("<slug:slug_category>/<slug:slug_product>/", ProductsViews.as_view(), name="product"),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
                static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
