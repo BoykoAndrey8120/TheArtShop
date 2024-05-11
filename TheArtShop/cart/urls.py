82a17714f0e18052c1b7148f5102a52bf83caa2a
andreyboyko <boyko8120@gmail.com>
2024-05-11 19:36:51 +0300

    added cart change

--- TheArtShop/cart/urls.py
+++ TheArtShop/cart/urls.py
@@ -5,31 +5,13 @@ from django.conf.urls.static import static
 from . import views
 from .views import cart_add
 
-# from .views import add_to_cart, view_cart, checkout
-
-
 app_name = 'cart'
-
-# urlpatterns = [
-#     # path('cart_add/<int:product_id>/', cart_add, name='cart_add'),
-#     # path('order/', view_cart, name='cart'),
-#     # path('checkout/', checkout, name='checkout'),
-#     path('cart/', views.cart_detail, name='cart_detail'),
-#     path('cart_add/', views.cart_add, name='cart_add'),
-#     # path(r'^remove/(?P<product_id>\d+)/$', views.cart_remove, name='cart_remove'),
-#
-# ]
 
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
     path('add/<int:product_id>/', views.cart_add, name='cart_add'),
     path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
 ]
-# urlpatterns = [
-#     path(r'^$', views.cart_detail, name='cart_detail'),
-#     path(r'^add/(?P<product_id>\d+)/$', views.cart_add, name='cart_add'),
-#     path(r'^remove/(?P<product_id>\d+)/$', views.cart_remove, name='cart_remove'),
-# ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
                static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- TheArtShop/site_content/urls.py
+++ TheArtShop/site_content/urls.py
@@ -1,18 +1,10 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.contrib.auth.views import LogoutView, LoginView
-from users.views import (CustomLoginView, CustomLogoutView, register)
 from .views import CategoryViews, ProductsViews, ProductView, Home
 
 
 urlpatterns = [
-    # path('register/', register, name='register'),
-    # path("login/", CustomLoginView.as_view(), name='login'),
-    # path('login/', views.login, name='login'),
-    # path('logout/', views.logout, name='logout'),
-    # path('logout/', CustomLogoutView.as_view(), name='logout'),
-    # path('logout/', LogoutView.as_view(), name='logout'),
     path("", Home.as_view(), name="home"),
     path("<slug:slug_category>", CategoryViews.as_view(), name="Category-detail"),
     path("<slug:slug_category>/products/page/<int:page>/", ProductsViews.as_view(), name='product-list-paginated'),
@@ -24,7 +16,3 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
                static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# urlpatterns = [
-#     #your url
-# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
